eac801d2d07d63029250e56f647bd90863ec3632
j-0-n-e-z <j000neeez@gmail.com>
2023-02-26 16:02:03 +0500

    add css variables for colors

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -14,16 +14,17 @@ export const Sidebar: FC<SidebarProps> = ({ steps, currentStepIndex }) => {
 				<div key={step} className={styles.step}>
 					<div
 						className={cn(styles.stepNum, {
-							[styles.currentStep]: stepIndex === currentStepIndex
+							[styles.currentStep]:
+								stepIndex === currentStepIndex ||
+								(stepIndex === steps.length - 1 &&
+									currentStepIndex > steps.length - 1)
 						})}
 					>
 						{stepIndex + 1}
 					</div>
 					<div className={styles.info}>
 						<div className={styles.title}>step {stepIndex + 1}</div>
-						<div className={styles.description}>
-							{step}
-						</div>
+						<div className={styles.description}>{step}</div>
 					</div>
 				</div>
 			))}
